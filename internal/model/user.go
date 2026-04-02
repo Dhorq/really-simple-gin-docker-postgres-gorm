7e47be19bdc9a0ece678e20d784bd667a717cb5c
Dhorq <ridhorifqy34@gmail.com>
2026-04-02 11:40:26 +0700

    feat: add jwt auth

---
+++ internal/model/user.go
@@ -0,0 +1,11 @@
+package model
+
+import "time"
+
+type User struct {
+	ID        uint      `gorm:"primaryKey" json:"id"`
+	Email     string    `gorm:"uniqueIndex;not null" json:"email"`
+	Password  string    `gorm:"not null" json:"-"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
---
+++ pkg/middleware/auth.go
@@ -0,0 +1,37 @@
+package middleware
+
+import (
+	"net/http"
+
+	"github.com/Dhorq/really-simple-gin-docker-postgres-gorm/pkg/auth"
+	"github.com/Dhorq/really-simple-gin-docker-postgres-gorm/pkg/response"
+	"github.com/gin-gonic/gin"
+)
+
+func AuthMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		tokenString := c.GetHeader("Authorization")
+
+		if tokenString == "" {
+			response.Error(c, http.StatusUnauthorized, "token is required")
+			c.Abort()
+			return
+		}
+
+		if len(tokenString) > 7 && tokenString[:7] == "Bearer " {
+			tokenString = tokenString[7:]
+		}
+
+		claims, err := auth.ValidateToken(tokenString)
+		if err != nil {
+			response.Error(c, http.StatusUnauthorized, "invalid token")
+			c.Abort()
+			return
+		}
+
+		c.Set("userID", claims.UserID)
+		c.Set("email", claims.Email)
+
+		c.Next()
+	}
+}
